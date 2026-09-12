890dabcdf30bd42cf5efbb01c7812d19d02ec622
José Vitor <93824107+josevitor10975@users.noreply.github.com>
2026-09-12 00:24:20 -0300

    style(home): enable overlay sidebar layout

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,10 +2,11 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppHeader } from "@/components/home/AppHeader";
 import { AppSidebar } from "@/components/home/AppSidebar";
 import { HomeContent } from "@/components/home/HomeContent";
+import styles from "./sidebar-overlay.module.css";
 
 export default function Home() {
   return (
-    <SidebarProvider>
+    <SidebarProvider className={styles.sidebarOverlay}>
       <AppSidebar />
       <SidebarInset>
         <AppHeader />
